6edc09bd1d4d18a4a73880b77f35f23a670890db
vladimir <v.khromov.work@gmail.com>
2025-10-09 22:07:20 +0300

    Add RSpec configuration and correct env file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'webmock/rspec'
+
+Dir[File.join(__dir__, '..', 'lib', '**', '*.rb')].sort.each { |file| require file }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  WebMock.disable_net_connect!(allow_localhost: true)
+
+  config.disable_monkey_patching!
+
+  config.order = :random
+end
