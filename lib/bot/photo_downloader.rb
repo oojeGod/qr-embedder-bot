818014663736a1a1c28c703b6a25edd239a08645
vladimir <v.khromov.work@gmail.com>
2025-10-10 23:48:15 +0300

    fix download logic

--- lib/bot/photo_downloader.rb
+++ lib/bot/photo_downloader.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'tempfile'
 require 'open-uri'
+require 'fileutils'
 
 module Bot
   # Downloads photos from Telegram servers
@@ -10,44 +10,47 @@ module Bot
 
     class << self
       def download(bot, file_id)
-      file_info = bot.api.get_file(file_id: file_id)
-      file_path = file_info.dig('result', 'file_path')
-      file_size = file_info.dig('result', 'file_size')
+        file_info = bot.api.get_file(file_id: file_id)
+        file_path = file_info.dig('result', 'file_path')
+        file_size = file_info.dig('result', 'file_size')
 
-      validate_file_size!(file_size)
+        validate_file_size!(file_size)
 
-      file_url = build_file_url(file_path)
-      download_to_tempfile(file_url, file_path)
-    rescue OpenURI::HTTPError, Timeout::Error => e
-      raise "Failed to download photo: #{e.message}"
+        file_url = build_file_url(file_path)
+        download_to_file(file_url, file_path)
+      rescue OpenURI::HTTPError, Timeout::Error => e
+        raise "Failed to download photo: #{e.message}"
       end
 
       private
 
       def validate_file_size!(file_size)
-      return unless file_size && file_size > MAX_FILE_SIZE
+        return unless file_size && file_size > MAX_FILE_SIZE
 
-      raise "File is too large (max #{MAX_FILE_SIZE / 1024 / 1024}MB)"
-    end
-
-    def build_file_url(file_path)
-      token = ENV.fetch('TELEGRAM_BOT_TOKEN')
-      "https://api.telegram.org/file/bot#{token}/#{file_path}"
-    end
-
-    def download_to_tempfile(file_url, file_path)
-      extension = File.extname(file_path)
-      extension = '.jpg' if extension.empty?
-
-      temp_file = Tempfile.new(['photo', extension])
-      temp_file.binmode
-
-      URI.open(file_url, 'rb', read_timeout: 30) do |remote_file|
-        temp_file.write(remote_file.read)
+        raise "File is too large (max #{MAX_FILE_SIZE / 1024 / 1024}MB)"
       end
 
-        temp_file.close
-        temp_file.path
+      def build_file_url(file_path)
+        token = ENV.fetch('TELEGRAM_BOT_TOKEN')
+        "https://api.telegram.org/file/bot#{token}/#{file_path}"
+      end
+
+      def download_to_file(file_url, file_path)
+        extension = File.extname(file_path)
+        extension = '.jpg' if extension.empty?
+
+        tmp_dir = ENV.fetch('TEMP_FOLDER', './tmp')
+        FileUtils.mkdir_p(tmp_dir) unless Dir.exist?(tmp_dir)
+
+        local_path = File.join(tmp_dir, "photo_#{Time.now.to_i}_#{rand(10000)}#{extension}")
+
+        File.open(local_path, 'wb') do |file|
+          URI.open(file_url, 'rb', read_timeout: 30) do |remote_file|
+            file.write(remote_file.read)
+          end
+        end
+
+        local_path
       end
     end
   end
