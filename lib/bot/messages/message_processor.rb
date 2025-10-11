fb33ee113f5f4f39031e3c516fd2de7a1b258bbf
vladimir <v.khromov.work@gmail.com>
2025-10-11 23:46:51 +0300

    add message processing logic

---
+++ lib/bot/messages/message_processor.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../user_state_manager'
+require_relative '../callbacks/response_builder'
+require_relative '../qr/vcard/vcard_builder'
+require_relative '../qr/qr_generator'
+require_relative 'message_handler'
+
+module Bot
+  module Messages
+    # Main orchestrator for message processing
+    class MessageProcessor
+      def initialize(bot, message)
+        @bot = bot
+        @message = message
+      end
+
+      def process
+        handler = Messages::MessageHandler.new(bot, message)
+        
+        return handler.handle_photo if message.photo
+        return handler.handle_text if message.text
+      end
+
+      private
+
+      attr_reader :bot, :message
+    end
+  end
+end
