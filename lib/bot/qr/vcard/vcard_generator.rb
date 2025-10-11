74a58302505c9c1fd59aa92d445738134aa85843
vladimir <v.khromov.work@gmail.com>
2025-10-11 23:44:14 +0300

    add vcard generator

---
+++ lib/bot/qr/vcard/vcard_generator.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../qr_generator'
+require_relative 'vcard_builder'
+require_relative '../user_state_manager'
+require_relative '../callbacks/response_builder'
+
+module Bot
+  module Qr
+    module Vcard
+      # Handles vCard QR code generation
+      class VcardGenerator
+        def initialize(bot, chat_id, vcard_data)
+          @bot = bot
+          @chat_id = chat_id
+          @vcard_data = vcard_data
+        end
+
+        def generate_vcard
+          vcard_content = VcardBuilder.build_vcard(vcard_data)
+          qr_generator = QrGenerator.new(bot, chat_id, vcard_content)
+          qr_generator.generate
+        end
+
+        private
+
+        attr_reader :bot, :chat_id, :vcard_data
+      end
+    end
+  end
+end
