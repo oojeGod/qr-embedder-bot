d81ff3b69218ad68f0152f2da050ec3ba0584e34
vladimir <v.khromov.work@gmail.com>
2025-10-13 23:08:46 +0300

    refactor tests for message processor logic

--- spec/bot/message_processor_spec.rb
+++ spec/bot/message_processor_spec.rb
@@ -2,102 +2,68 @@
 
 require 'spec_helper'
 
-RSpec.describe Bot::Messages::MessageProcessor do
+RSpec.describe Bot::Messages::Processor do
   subject(:processor) { described_class.new(bot: bot, message: message) }
 
   let(:bot) { double('bot', api: api) }
   let(:api) { double('api') }
   let(:chat_id) { 12_345 }
-  let(:message) { double('message', chat: double('chat', id: chat_id)) }
+  let(:chat) { double('chat', id: chat_id) }
+  let(:handler) { instance_double(Bot::Messages::Handler) }
 
   before do
-    ENV['TELEGRAM_BOT_TOKEN'] = 'test_token'
+    allow(Bot::Messages::Handler).to receive(:new)
+      .with(bot: bot, message: message)
+      .and_return(handler)
+  end
+
+  after do
+    Bot::UserStateManager.cleanup(chat_id: chat_id)
   end
 
   describe '#process' do
-    context 'with photo' do
-      before do
-        allow(message).to receive(:photo).and_return([double(file_id: 'photo_123')])
-        allow(message).to receive(:text).and_return(nil)
-      end
+    context 'with photo message' do
+      let(:photo) { [double('photo', file_id: 'photo_123')] }
+      let(:message) { double('message', chat: chat, photo: photo, text: nil) }
 
-      it 'sends QR type selection' do
-        expect(api).to receive(:send_message)
+      it 'delegates to handler.handle_photo' do
+        expect(handler).to receive(:handle_photo)
         processor.process
       end
     end
 
-    context 'with text' do
-      before do
-        allow(message).to receive(:photo).and_return(nil)
-        allow(message).to receive(:text).and_return('https://example.com')
-      end
+    context 'with text message' do
+      let(:message) { double('message', chat: chat, photo: nil, text: '/start') }
 
-      context 'without photo' do
-        it 'asks for photo' do
-          expect(api).to receive(:send_message).with(hash_including(text: /photo first/i))
-          processor.process
-        end
-      end
-
-      context 'with photo and QR type selected' do
-        before do
-          submit_photo_and_select_qr_type
-          stub_processing
-        end
-
-        it 'processes and sends result', :aggregate_failures do
-          expect(api).to receive(:send_message).with(hash_including(text: /Processing/i))
-          expect(api).to receive(:send_photo).with(hash_including(caption: /Done/i))
-          
-          processor.process
-        end
-      end
-    end
-  end
-
-  describe '#process_callback' do
-    let(:callback) { double('callback', id: 'cbq', data: 'url', message: message) }
-
-    context 'with photo state' do
-      before do
-        submit_photo
-        allow(api).to receive(:answer_callback_query)
-      end
-
-      it 'prompts for data' do
-        expect(api).to receive(:send_message).with(hash_including(text: /URL/i))
-        processor.process_callback(callback, bot)
+      it 'delegates to handler.handle_text' do
+        expect(handler).to receive(:handle_text)
+        processor.process
       end
     end
 
-    context 'without photo state' do
-      it 'ignores callback' do
-        expect(api).not_to receive(:send_message)
-        processor.process_callback(callback, bot)
+    context 'with message containing both photo and text' do
+      let(:photo) { [double('photo', file_id: 'photo_123')] }
+      let(:message) { double('message', chat: chat, photo: photo, text: 'caption') }
+
+      it 'prioritizes photo handling' do
+        expect(handler).to receive(:handle_photo)
+        expect(handler).not_to receive(:handle_text)
+        processor.process
       end
     end
-  end
 
-  private
+    context 'with message containing neither photo nor text' do
+      let(:message) { double('message', chat: chat, photo: nil, text: nil) }
 
-  def submit_photo
-    photo_msg = double('msg', chat: double('chat', id: chat_id), photo: [double(file_id: 'photo_123')], text: nil)
-    allow(api).to receive(:send_message)
-    described_class.new(bot: bot, message: photo_msg).process
-  end
+      it 'does not call any handler' do
+        expect(handler).not_to receive(:handle_photo)
+        expect(handler).not_to receive(:handle_text)
+        processor.process
+      end
 
-  def submit_photo_and_select_qr_type
-    submit_photo
-    callback = double('callback', id: 'cbq', data: 'url', message: double('msg', chat: double('chat', id: chat_id)))
-    allow(api).to receive(:answer_callback_query)
-    processor.process_callback(callback, bot)
-  end
-
-  def stub_processing
-    allow(Bot::PhotoDownloader).to receive(:new).and_return(double(download: '/tmp/photo.jpg'))
-    allow(Services::ImageProcessor).to receive(:new).and_return(double(process: '/tmp/result.png'))
-    allow(File).to receive(:exist?).and_return(false)
-    allow(File).to receive(:open).and_return(double('file'))
+      it 'returns nil' do
+        expect(processor.process).to be_nil
+      end
+    end
   end
 end
